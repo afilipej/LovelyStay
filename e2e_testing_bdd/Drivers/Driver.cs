3b5278c96c8eda9da718d454ae1943e14df8b595
andre.filipe.jardim@gmail.com <andre.filipe.jardim@gmail.com>
2024-03-11 09:04:28 +0000

    first commit

---
+++ e2e_testing_bdd/Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace e2e_testing_bdd.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ e2e_testing_bdd/Hooks/Hook.cs
@@ -0,0 +1,10 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace e2e_testing_bdd.Hooks
+{
+    [Binding]
+    public class Hooks
+    {
+    }
+}
